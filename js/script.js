aa6f99d20955e9efbb8f29298dd56ffaa8785e6d
Ethan Suttner <ethan.suttner@gmail.com>
2016-09-05 14:03:41 -0400

    create color buttons and supporting functions

--- js/script.js
+++ js/script.js
@@ -1,10 +1,30 @@
 $(document).ready(function() {
-  //var gameOn = false;
+  var gameOn = false;
+  const buttons = ['.red-button', '.yellow-button', '.blue-button', '.green-button'];
+  var buttonHistory = [];
+  var currentIndex = 0;
 
   $(".on-off-button").click(function() {
-  	alert("Game On!");
+  	console.log("Game On!");
   });
+
+  $(".color-button").click(function() {
+  	console.log($(this)[0].id);
+  });
+
+  function colorButtonsDisabled(bool) {
+  	$(".color-button").prop("disabled", bool); 
+  }
+
+  function randomButton() {
+  	return buttons[getRandomIntInclusive(0,3)];
+  }
+  // Returns a random integer between min (included) and max (included)
+	// Using Math.round() will give you a non-uniform distribution!
+  function getRandomIntInclusive(min, max) {
+  	min = Math.ceil(min);
+  	max = Math.floor(max);
+  	return Math.floor(Math.random() * (max - min + 1)) + min;
+	}
+
 });
-// $(function(){
-//   alert("My First Jquery Test");
-// });
