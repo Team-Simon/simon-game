59e1ae79d145bd855385f635d89e45dc30dc541c
Ethan Suttner <ethan.suttner@gmail.com>
2016-09-05 09:52:47 -0400

    add jquery

---
+++ js/script.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+  //var gameOn = false;
+
+  $(".on-off-button").click(function() {
+  	alert("Game On!");
+  });
+});
+// $(function(){
+//   alert("My First Jquery Test");
+// });
